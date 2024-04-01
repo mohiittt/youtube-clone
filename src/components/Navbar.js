1d6ea55bfcb2ff62f8cd1c374695d4458b2e63b6
mohiittt <mohitpatil.np@gmail.com>
2024-04-01 11:47:12 +0530

    Added Search Page & SearchCard

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,61 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { IoLogoYoutube } from "react-icons/io";
+import { BsYoutube } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMicrophone } from "react-icons/fa";
 import { RiVideoAddLine } from "react-icons/ri";
-import { FaRegBell } from "react-icons/fa";
-import { FaRegCircleUser } from "react-icons/fa6";
+import { BsBell } from "react-icons/bs";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { useAppDispatch, useAppSelector } from "../hooks/useApp";
+import {
+  changeSearchTerm,
+  clearVideos,
+} from "../features/youtube/youtubeSlice";
 
-const Navbar = () => {
+export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
+
+  const handleSearch = () => {
+    if (location.pathname !== "/search") navigate("/search");
+    else {
+      dispatch(clearVideos);
+      dispatch(getSearchPageVideos(false));
+    }
+  };
+
   return (
-    <div className="px-14 h-14 flex justify-between items-center bg-[#212121] opacity-95 sticky ">
+    <div className="flex justify-between items-center px-14 h-14  bg-[#212121] opacity-95 sticky top-0 z-50">
       <div className="flex gap-8 items-center text-2xl">
         <div>
           <GiHamburgerMenu />
         </div>
         <div className="flex gap-1 items-center justify-center">
-          <IoLogoYoutube className="text-3xl text-red-600" />
-          <span className="text-2xl ">YouTube</span>
+          <BsYoutube className="text-3xl text-red-600" />
+          <span className="text-2xl ">Youtube</span>
         </div>
       </div>
       <div className="flex items-center justify-center gap-5">
-        <form>
-          <div className="flex bg-zinc-900 items-center h-10 px-4 pr-2 rounded-3xl">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
+        >
+          <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0 rounded-3xl">
             <div className="flex gap-5 items-center pr-5">
               <input
                 type="text"
                 placeholder="Search"
                 className="w-96 bg-zinc-900 focus:outline-none border-none"
+                value={searchTerm}
+                onChange={(e) => dispatch(changeSearchTerm(e.target.value))}
               />
             </div>
-            <button className="h-10 w-16 flex items-center justify-center bg-zinc-900 rounded-r-3xl">
+            <button className="h-10 w-16 flex items-center justify-center bg-zinc-800 rounded-r-3xl">
               <AiOutlineSearch className="text-xl " />
             </button>
           </div>
@@ -39,23 +65,21 @@ const Navbar = () => {
           <FaMicrophone />
         </div>
       </div>
-
-      <div className="flex justify-center items-center gap-8">
-        <div className="flex gap-2 items-center text-xl">
-          <RiVideoAddLine />
-        </div>
+      <div className="flex gap-8 items-center text-xl">
+        <RiVideoAddLine />
         <div className="relative">
-          <FaRegBell />
+          <BsBell />
           <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
-            9+
+            {" "}
+            9+{" "}
           </span>
         </div>
-        <div>
-          <FaRegCircleUser />
-        </div>
+        <img
+          src="https://yt3.googleusercontent.com/Ad8Q8PblBvMbfVEEpuSiAvcLoI6XqEecc4GEjCN41716Zc1lLEjR6lPB4JXjKzMLVFYskARDaNU=s900-c-k-c0x00ffffff-no-rj"
+          alt="profile logo"
+          className="w-9 h-9 rounded-full"
+        />
       </div>
     </div>
   );
-};
-
-export default Navbar;
+}
--- src/features/youtube/youtubeSlice.js
+++ src/features/youtube/youtubeSlice.js
@@ -1,11 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getHomePageVideos } from "../../store/reducers/getHomePageVideos";
+import { getSearchPageVideos } from "../../store/reducers/getSearchPageVideos";
+import { getRecommendedVideos } from "../../store/reducers/getRecommendedVideos";
+import { getVideoDetails } from "../../store/reducers/getVideoDetails";
 
 const initialState = {
   videos: [],
   currentPlaying: null,
-  serachTerm: "",
-  serachResults: [],
+  searchTerm: "",
+  searchResults: [],
   nextPageToken: null,
   recommendedVideo: [],
 };
@@ -13,7 +16,18 @@ const initialState = {
 const youtubeSlice = createSlice({
   name: "youtubeApp",
   initialState,
-  reducers: {},
+  reducers: {
+    clearVideos: (state) => {
+      state.videos = [];
+      state.nextPageToken = null;
+    },
+    changeSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+    clearSearchTerm: (state) => {
+      state.searchTerm = "";
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getHomePageVideos.fulfilled, (state, action) => {
       if (action.payload && action.payload.parsedData) {
@@ -21,7 +35,23 @@ const youtubeSlice = createSlice({
         state.nextPageToken = action.payload.nextPageToken;
       }
     });
+    builder.addCase(getSearchPageVideos.fulfilled, (state, action) => {
+      if (action.payload && action.payload.parsedData) {
+        state.videos = action.payload.parsedData;
+        state.nextPageToken = action.payload.nextPageToken;
+      }
+    });
+    builder.addCase(getRecommendedVideos.fulfilled, (state, action) => {
+      if (action.payload && action.payload.parsedData) {
+        state.recommendedVideo = action.payload.parsedData;
+      }
+    });
+    builder.addCase(getVideoDetails.fulfilled, (state, action) => {
+      state.currentPlaying = action.payload;
+    });
   },
 });
 
+export const { clearVideos, changeSearchTerm, clearSearchTerm } =
+  youtubeSlice.actions;
 export default youtubeSlice.reducer;
